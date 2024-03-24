5ed3b3327a9d3ba5eaad26b7de1ca509746ff398
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-03-24 19:51:28 +0100

    created first files

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Navbar.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+
+    </div>
+  )
+}
+
+export default Navbar
