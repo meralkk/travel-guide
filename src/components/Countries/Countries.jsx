42f659be7216ea99435f41968d6261c0dd56f14a
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-04-02 20:38:05 +0200

    Update Countries component to display cities with scrolling feature; add CSS for new components

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,10 +1,12 @@
-import React from "react";
 import "./Countries.css";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import dresden from "../../assets/germany/germany.jpg";
+import germany from '../../assets/germany/germany.jpg'
+import Spacer from "../Spacer/Spacer";
 
 const Countries = () => {
+
+
   return (
     <Container className="countries">
       <Row>
@@ -16,11 +18,82 @@ const Countries = () => {
         <Col>
           <div className="menu">
             <ul>
-              <li>Deutschland, Greece, Georgia, Czechia, Turkey</li>
+            <li><a href="#germany">Germany</a>,</li>
+            <li><a href="#greece">Greece</a>,</li>
+            <li><a href="#turkey">Turkey</a>,</li>
+              <li>Georgia,</li>
             </ul>
           </div>
         </Col>
       </Row>
+      <Row>
+        <Col id="germany">
+        <Spacer/>
+        <h1>#Germany</h1>
+        <Spacer/>
+        <div className="cities">
+          <Col><h2>Dresden</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Hamburg</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Berlin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Schwerin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Schwerin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+
+
+        </div>
+        </Col>
+        
+      </Row>
+      <Row>
+        <Col id="greece">
+        <Spacer/>
+        <h1>#Greece</h1>
+        <Spacer/>
+        <div className="cities">
+          <Col><h2>Dresden</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Hamburg</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Berlin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Schwerin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Schwerin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+
+
+        </div>
+        </Col>
+        
+      </Row>
+      <Row>
+        <Col id="turkey">
+        <Spacer/>
+        <h1>#Turkey</h1>
+        <Spacer/>
+        <div className="cities">
+          <Col><h2>Dresden</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Hamburg</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Berlin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Schwerin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+          <Col><h2>Schwerin</h2>
+          <img className="cities-image" src={germany} alt="" /></Col>
+
+
+        </div>
+        </Col>
+        
+      </Row>
+
+
     </Container>
   );
 };
