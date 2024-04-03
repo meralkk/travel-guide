bd5a84dda279ad9c45acb9a1169a238dc30f9e35
Meral Keskin Kubilay <52655033+meralkk@users.noreply.github.com>
2024-04-03 20:13:49 +0200

    Update Countries component with city links and fix Navbar width

--- src/components/Countries/Countries.jsx
+++ src/components/Countries/Countries.jsx
@@ -1,12 +1,10 @@
 import "./Countries.css";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import germany from '../../assets/germany/germany.jpg'
+import germany from "../../assets/germany/germany.jpg";
 import Spacer from "../Spacer/Spacer";
 
 const Countries = () => {
-
-
   return (
     <Container className="countries">
       <Row>
@@ -18,9 +16,15 @@ const Countries = () => {
         <Col>
           <div className="menu">
             <ul>
-            <li><a href="#germany">Germany</a>,</li>
-            <li><a href="#greece">Greece</a>,</li>
-            <li><a href="#turkey">Turkey</a>,</li>
+              <li>
+                <a href="#germany">Germany</a>,
+              </li>
+              <li>
+                <a href="#greece">Greece</a>,
+              </li>
+              <li>
+                <a href="#turkey">Turkey</a>,
+              </li>
               <li>Georgia,</li>
             </ul>
           </div>
@@ -28,72 +32,93 @@ const Countries = () => {
       </Row>
       <Row>
         <Col id="germany">
-        <Spacer/>
-        <h1>#Germany</h1>
-        <Spacer/>
-        <div className="cities">
-          <Col><h2>Dresden</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Hamburg</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Berlin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Schwerin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Schwerin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-
-
-        </div>
+          <Spacer />
+          <h1>#Germany</h1>
+          <Spacer />
+          <div className="cities">
+            <Col>
+              <a href="/destinations/germany/dresden">
+                <h2>Dresden</h2>
+                <img className="cities-image" src={germany} alt="" />
+              </a>
+            </Col>
+            <Col>
+              <h2>Hamburg</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Berlin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Schwerin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Schwerin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+          </div>
         </Col>
-        
       </Row>
       <Row>
         <Col id="greece">
-        <Spacer/>
-        <h1>#Greece</h1>
-        <Spacer/>
-        <div className="cities">
-          <Col><h2>Dresden</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Hamburg</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Berlin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Schwerin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Schwerin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-
-
-        </div>
+          <Spacer />
+          <h1>#Greece</h1>
+          <Spacer />
+          <div className="cities">
+            <Col>
+              <h2>Dresden</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Hamburg</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Berlin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Schwerin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Schwerin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+          </div>
         </Col>
-        
       </Row>
       <Row>
         <Col id="turkey">
-        <Spacer/>
-        <h1>#Turkey</h1>
-        <Spacer/>
-        <div className="cities">
-          <Col><h2>Dresden</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Hamburg</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Berlin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Schwerin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-          <Col><h2>Schwerin</h2>
-          <img className="cities-image" src={germany} alt="" /></Col>
-
-
-        </div>
+          <Spacer />
+          <h1>#Turkey</h1>
+          <Spacer />
+          <div className="cities">
+            <Col>
+              <h2>Dresden</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Hamburg</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Berlin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Schwerin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+            <Col>
+              <h2>Schwerin</h2>
+              <img className="cities-image" src={germany} alt="" />
+            </Col>
+          </div>
         </Col>
-        
       </Row>
-
-
     </Container>
   );
 };
